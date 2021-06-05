7cafbcbfec1cbfec934d571b2ad57d5d2615a384
Jef Wright <thejefwright@Jefs-MacBook-Pro-2.local>
2021-06-05 13:52:54 -0400

    still trying to get clip path to work with safari

--- components/MobileNav.js
+++ components/MobileNav.js
@@ -3,19 +3,10 @@ import styled from 'styled-components';
 import Hamburger from './Hamburger';
 import Nav from './Nav';
 
-// const ShadowStyles = styled.div`
-//   filter: drop-shadow(-1px 1px 2px rgba(255, 255, 255, 0.3));
-//   position: fixed;
-//   bottom: 1rem;
-//   width: 100vw;
-//   left: 50%;
-//   top: 0;
-//   transform: translateX(-50%);
-// `;
 const MobileStyles = styled.div`
   position: fixed;
   top: 0;
-  bottom: 1em;
+  bottom: 2rem;
   height: 100vh;
   width: 100%;
   background: var(--pop);
@@ -23,11 +14,12 @@ const MobileStyles = styled.div`
   font-size: 2rem;
   clip-path: circle(30px at 50% 95%);
   -webkit-clip-path: circle(30px at 50% 95%);
+  pointer-events: none;
   box-shadow: -1px 1px 5px wheat;
   z-index: 1;
   ${(props) => props.open && `clip-path: circle(100% at 50% 50%);`};
   ${(props) => props.open && `-webkit-clip-path: circle(100% at 50% 50%);`};
-  ${(props) => props.open && `box-shadow: none;`};
+  ${(props) => props.open && `pointer-events: auto;`};
   transition: all 0.4s;
 
   .wrapper {
