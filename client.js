ca30e2693ee3c5e2cacc7f727569d7afea8bb651
Jef Wright <thejefwright@Jefs-MacBook-Pro-2.local>
2021-08-09 11:21:42 -0400

    removing coments

--- client.js
+++ client.js
@@ -2,7 +2,7 @@ import sanityClient from '@sanity/client';
 
 export default sanityClient({
   projectId: '9vv91r16',
-  dataset: 'production', // or the name you chose in step 1
-  useCdn: true, // `false` if you want to ensure fresh data
+  dataset: 'production',
+  useCdn: true,
   watchMode: true,
 });
