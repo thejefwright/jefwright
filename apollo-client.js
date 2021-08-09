54c2980dbb892bb3a00c4b6368cc465f4c437a56
Jef Wright <thejefwright@Jefs-MacBook-Pro-2.local>
2021-08-09 11:21:24 -0400

    removing apollo

--- apollo-client.js
+++ apollo-client.js
@@ -1,8 +1,8 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+// import { ApolloClient, InMemoryCache } from '@apollo/client';
 
-const client = new ApolloClient({
-  uri: 'https://9vv91r16.api.sanity.io/v1/graphql/production/default',
-  cache: new InMemoryCache(),
-});
+// const client = new ApolloClient({
+//   uri: 'https://9vv91r16.api.sanity.io/v1/graphql/production/default',
+//   cache: new InMemoryCache(),
+// });
 
-export default client;
+// export default client;
